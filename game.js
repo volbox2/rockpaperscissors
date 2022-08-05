0b614636e3fb1c95b274241aad57136980c79904
volbox2 <ngson1902@gmail.com>
2022-08-05 16:26:28 +0200

    Done first part of Rock,Paper,Scissors

--- game.js
+++ game.js
@@ -10,3 +10,35 @@ function getComputerChoice(){
 function getRandomNumber(min,max){
     return Math.floor(Math.random()*(max-min+1))+min;
 }
+
+function playRound(playerSelection,computerSelection){
+
+    playerSelection = playerSelection.toLowerCase()
+    if (computerSelection==="Rock"){
+        if (playerSelection==="rock")  {console.log("It's a draw")}
+        else if(playerSelection==="scissors") {console.log("You Lose! Rock beats Scissors!")}
+        else {console.log("You Win! Paper beats Rock!")}
+    }
+
+    else if(computerSelection==="Scissors"){
+        if (playerSelection==="rock")  {console.log("You Win! Rock beats Scissors!")}
+        else if(playerSelection==="scissors") {console.log("It's a draw")}
+        else {console.log("You Lose! Scissors beat Paper!")}
+    }
+
+    else{
+        if (playerSelection==="rock")  {console.log("You Lose! Paper beats Rock!")}
+        else if(playerSelection==="scissors") {console.log("You Win! Scissors beat Paper!")}
+        else {console.log("It's a draw")}
+    }
+}
+
+
+function game(){
+    for (let i=0;i<5;++i){
+        let player= prompt("Let's play a game of Rock,Paper,Scissors! What do you want to play?")
+        playRound(player,getComputerChoice())
+    }
+}
+
+game()
